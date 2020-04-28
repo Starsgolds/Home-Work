930dbdf7c418c29943e9d7a78202b4282d27d640
Starsgolds <61186709+Starsgolds@users.noreply.github.com>
2020-04-28 21:59:49 +0600

    Update 11.js

--- Set_WeakSet/11/11.js
+++ Set_WeakSet/11/11.js
@@ -1,10 +1,15 @@
-let button = document.querySelector('button'); 
-let elems = document.querySelectorAll('p'); 
 let set = new Set; 
+let elems = document.querySelectorAll('p'); 
+let btn = document.querySelector('button'); 
 for(let elem of elems){ 
-elem.onclick = () => set.add(elem); 
+elem.addEventListener('click', function () { 
+set.add(this); 
+console.log(set); 
+}); 
 } 
-button.onclick = () => { 
-set.forEach(elem => elem.innerHTML += '!'); 
+btn.addEventListener('click', function () { 
+for(let elem of set){ 
+elem.innerHTML += '!'; 
+} 
 set.clear(); 
-}
+});
