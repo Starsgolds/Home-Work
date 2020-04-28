fe9d8d73cac2411d17c10ffbf750474e5873c838
Starsgolds <61186709+Starsgolds@users.noreply.github.com>
2020-04-28 22:19:09 +0600

    Update 9.js

--- Set_WeakSet/9/9.js
+++ Set_WeakSet/9/9.js
@@ -1,6 +1,6 @@
-let input = document.querySelector('#input'); 
-let button = document.querySelector('#button'); 
-let text = document.querySelector('p'); 
+let input = document.querySelector("input"); 
+let button = document.querySelector("button"); 
+let text = document.querySelector("p"); 
 let set = new Set; 
 button.addEventListener('click', function(){ 
 let newNum = +input.value; 
