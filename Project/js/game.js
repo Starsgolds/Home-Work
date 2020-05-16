3f4477e5d9991c7539e147378ade2fbc6e12f2ff
Starsgolds <61186709+Starsgolds@users.noreply.github.com>
2020-05-16 21:25:14 +0600

    Update game.js

--- Project/js/game.js
+++ Project/js/game.js
@@ -51,15 +51,14 @@ function draw() {
  y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
  });
  }
+//отслеж препятств
 
- // Отслеживание препятствий
- if(xPos + bird.width >= pipe[i].x
+if(xPos + bird.width >= pipe[i].x
  && xPos <= pipe[i].x + pipeUp.width
  && (yPos <= pipe[i].y + pipeUp.height
  || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
  location.reload(); // Перезагрузка страницы
  }
-
  if(pipe[i].x == 5) {
  score++;
  }
