c373b1f46d9a563e131b664a4c580314a81826fa
Starsgolds <61186709+Starsgolds@users.noreply.github.com>
2020-05-17 00:18:39 +0600

    Update game.js

--- Project/js/game.js
+++ Project/js/game.js
@@ -6,14 +6,14 @@ var ctx = cvs.getContext("2d");
 var bird = new Image();
 var bg = new Image();
 var fg = new Image();
-var pipeNorth = new Image();
-var pipeSouth = new Image();
+var pipeUp = new Image();
+var pipeBottom = new Image();
 
 bird.src = "images/bird.png";
 bg.src = "images/bg.png";
 fg.src = "images/fg.png";
-pipeNorth.src = "images/pipeNorth.png";
-pipeSouth.src = "images/pipeSouth.png";
+pipeUp.src = "images/pipeUp.png";
+pipeBottom.src = "images/pipeBottom.png";
 
 // some variables
 
@@ -27,6 +27,8 @@ var gravity = 1.5;
 
 var score = 0;
 
+// audio files
+
 
 // on key down
 
@@ -34,6 +36,7 @@ document.addEventListener("keydown",moveUp);
 
 function moveUp(){
     bY -= 25;
+    fly.play();
 }
 
 // pipe coordinates
@@ -55,27 +58,27 @@ function draw(){
     for(var i = 0; i < pipe.length; i++){
         
         constant = pipeNorth.height+gap;
-        ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
-        ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
+        ctx.drawImage(pipeUp,pipe[i].x,pipe[i].y);
+        ctx.drawImage(pipeBottom,pipe[i].x,pipe[i].y+constant);
              
         pipe[i].x--;
         
         if( pipe[i].x == 125 ){
             pipe.push({
                 x : cvs.width,
-                y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
+                y : Math.floor(Math.random()*pipeUp.height)-pipeUp.height
             }); 
         }
 
         // detect collision
         
-        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
+        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeUp.width && (bY <= pipe[i].y + pipeUp.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
             location.reload(); // reload the page
         }
         
         if(pipe[i].x == 5){
             score++;
-            
+            scor.play();
         }
         
         
