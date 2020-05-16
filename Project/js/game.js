1a98978c65ba62e14cbcd45ef6b0048f578e6f1c
Starsgolds <61186709+Starsgolds@users.noreply.github.com>
2020-05-17 00:12:39 +0600

    Update game.js

--- Project/js/game.js
+++ Project/js/game.js
@@ -1,65 +1,98 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
-var bird = new Image();
-var bg = new Image(); //задний фон
-var fg = new Image(); //передний фон
-var pipeUp = new Image(); 
-var pipeBottom = new Image();
 
-bird.src = "img/bird.png";
-bg.src = "img/bg.png";
-fg.src = "img/fg.png";
-pipeUp.src = "img/pipeUp.png";
-pipeBottom.src = "img/pipeBottom.png";
+// load images
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeNorth = new Image();
+var pipeSouth = new Image();
+
+bird.src = "images/bird.png";
+bg.src = "images/bg.png";
+fg.src = "images/fg.png";
+pipeNorth.src = "images/pipeNorth.png";
+pipeSouth.src = "images/pipeSouth.png";
+
+// some variables
 
 var gap = 85;
 var constant;
-var bX=10;
-var bY=150;
-var gravity=1,5;
-var score=0;
-//нажатие на кнопку
-document.addEventListener("keydown", moveUp);
 
-function moveUp() {
- bY -= 25;
+var bX = 10;
+var bY = 150;
+
+var gravity = 1.5;
+
+var score = 0;
+
+
+// on key down
+
+document.addEventListener("keydown",moveUp);
+
+function moveUp(){
+    bY -= 25;
 }
 
-// создание блоков
+// pipe coordinates
+
 var pipe = [];
 
 pipe[0] = {
- x : cvs.width;
- y : 0
+    x : cvs.width,
+    y : 0
+};
+
+// draw images
+
+function draw(){
+    
+    ctx.drawImage(bg,0,0);
+    
+    
+    for(var i = 0; i < pipe.length; i++){
+        
+        constant = pipeNorth.height+gap;
+        ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
+        ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
+             
+        pipe[i].x--;
+        
+        if( pipe[i].x == 125 ){
+            pipe.push({
+                x : cvs.width,
+                y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
+            }); 
+        }
+
+        // detect collision
+        
+        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
+            location.reload(); // reload the page
+        }
+        
+        if(pipe[i].x == 5){
+            score++;
+            
+        }
+        
+        
+    }
+
+    ctx.drawImage(fg,0,cvs.height - fg.height);
+    
+    ctx.drawImage(bird,bX,bY);
+    
+    bY += gravity;
+    
+    ctx.fillStyle = "#000";
+    ctx.font = "20px Verdana";
+    ctx.fillText("Score : "+score,10,cvs.height-20);
+    
+    requestAnimationFrame(draw);
+    
 }
 
-
-function draw() {
- ctx.drawImage(bg, 0, 0);
-
- for(var i = 0; i < pipe.length; i++) {
-constant=pipeUp.height+gap;
- ctx.drawImage(pipeUp,pipe[i].x,pipe[i].y);
- ctx.drawImage(pipeBottom,pipe[i].x,pipe[i].y+constant);
-pipe[i].x--;
-if (pipe[i].x==125) {
-pipe.push({
-x:cvs.width,
-y:Math.floor(Math.random()*pipeUp.height)-pipeUp.height
-});
-}
-if(bX+bird.width>=pipe[i].x&&bX<=pipe[i].x+pipeUp.width&&(bY<=pipe[i].y+pipeUp.height||bY+bird.height>=pipe[i].y+constant)||bY+bird.height>=cvs.height-fg.height) {
-location.reload();
-}
-if (pipe[i].x==5) {
-score++;
-}
-ctx.drawImage(fg,0,cvs.height-fg.height);
-ctx.drawImage(bird,bX,bY);
-bY+=gravity;
-ctx.fillStyle = "#000";
-ctx.font = "24px Verdana";
-ctx.fillText("Игровой счет: " + score, 10, cvs.height-20);
-requestAnimationFrame(draw);
-}
 draw();
